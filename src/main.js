5beb239f2861d5caf09af4a9f61665daaee757ca
Kunal Kataria <kkataria@usc.edu>
2017-09-24 21:33:17 -0700

    working better now, still need to add search results from github

--- src/main.js
+++ src/main.js
@@ -2,15 +2,43 @@ function fetchReposForUsername(username) {
   return $.getJSON('https://api.github.com/users/' + username + '/repos');
 }
 
-$('#search-button').on('click', function() {
-  var searchTemplate = $('#search-template').html();
-  var renderSearches = Handlebars.compile(searchTemplate);
+var searchTemplate = $('#searches-template').html();
+var renderSearches = Handlebars.compile(searchTemplate);
 
+// var repoTemplate = $('#repos-template').html();
+// var renderRepos = Handlebars.compile(repoTemplate);
+
+let searches = [];
+
+$.ajax({
+  type: 'GET',
+  url: 'http://localhost:3000/api/searches'
+}).then(function(response) {
+  $('#search-history').append(renderSearches({
+    searches: response
+  }));
+  searches = response;
+  console.log(response);
+});
+
+$('#search-button').on('click', function() {
   var username = $('#search-input').val();
 
-  fetchReposForUsername(username).then(function(response) {
+  $.ajax({
+    type: 'post',
+    url: 'http://localhost:3000/api/searches',
+    data: {
+      term: username,
+      createdAt: new Date()
+    }
+  }).then(function(response) {
+    searches.push(response);
 
+  });
+
+  fetchReposForUsername(username).then(function(response) {
+    console.log(response);
   }, function() {
-    
+
   });
 });
